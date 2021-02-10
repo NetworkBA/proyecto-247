9a6555dc82242fdac912d5c256c5ab764756c2c8
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-09 19:53:53 -0600

    x

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,7 @@
 <body id="page-top">
 
 
-         <div class="d-flex flex-column" id="content-wrapper">
-            <div id="content">
-            </div>
-            <footer class="bg-white sticky-footer">
-                <div class="container my-auto">
-                    <div class="text-center my-auto copyright"><span>Copyright © Proyecto-256 2021</span></div>
-                </div>
-            </footer>
-        </div>
-        <a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
-    </div>
+        
 
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,60 +45,70 @@
           </div>
       </nav>
       <div class="d-flex flex-column" id="content-wrapper">
-          <div id="content">
-        <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
+        <div id="content">
+            <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
 
-            <div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle mr-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
+                <div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle mr-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
 
-                <ul class="nav navbar-nav flex-nowrap ml-auto">
+                    <ul class="nav navbar-nav flex-nowrap ml-auto">
 
-                  @guest
-                      @if (Route::has('login'))
-                          <li class="nav-item">
-                              <a class="nav-link" href="{{ route('login') }}">{{ __('Inicio de Sesión') }}</a>
-                          </li>
-                      @endif
+                      @guest
+                          @if (Route::has('login'))
+                              <li class="nav-item">
+                                  <a class="nav-link" href="{{ route('login') }}">{{ __('Inicio de Sesión') }}</a>
+                              </li>
+                          @endif
 
-                      @if (Route::has('register'))
-                          <li class="nav-item">
-                              <a class="nav-link" href="{{ route('register') }}">{{ __('Registrar') }}</a>
-                          </li>
-                      @endif
-                  @else
-
-
-                  <div class="d-none d-sm-block topbar-divider"></div>
-                  <li class="nav-item dropdown no-arrow">
-                      <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#"><span class="d-none d-lg-inline mr-2 text-gray-600 small">{{ Auth::user()->name }}</span><img class="border rounded-circle img-profile" src="assets/img/avatars/avatar1.jpeg"></a>
-                          <div class="dropdown-menu shadow dropdown-menu-right animated--grow-in"><a class="dropdown-item" href="#"><i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Perfil</a><a class="dropdown-item" href="#"><i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Ajustes</a><a class="dropdown-item" href="#">
-                            <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Activity log</a>
-
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                 onclick="event.preventDefault();
-                                               document.getElementById('logout-form').submit();">
-                                  {{ __('Cerrar Sesión') }}
-                              </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                          </div>
-                          </div>
-                  </li>
-
-                  @endguest
+                          @if (Route::has('register'))
+                              <li class="nav-item">
+                                  <a class="nav-link" href="{{ route('register') }}">{{ __('Registrar') }}</a>
+                              </li>
+                          @endif
+                      @else
 
 
+                      <div class="d-none d-sm-block topbar-divider"></div>
+                      <li class="nav-item dropdown no-arrow">
+                          <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#"><span class="d-none d-lg-inline mr-2 text-gray-600 small">{{ Auth::user()->name }}</span><img class="border rounded-circle img-profile" src="assets/img/avatars/avatar1.jpeg"></a>
+                              <div class="dropdown-menu shadow dropdown-menu-right animated--grow-in"><a class="dropdown-item" href="#"><i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Perfil</a><a class="dropdown-item" href="#"><i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Ajustes</a><a class="dropdown-item" href="#">
+                                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Activity log</a>
+
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                     onclick="event.preventDefault();
+                                                   document.getElementById('logout-form').submit();">
+                                      {{ __('Cerrar Sesión') }}
+                                  </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                              </div>
+                              </div>
+                      </li>
+
+                      @endguest
 
 
-                </ul>
-            </div>
-        </nav>
-      </div>
+
+
+                    </ul>
+                </div>
+            </nav>
+        </div>
 
       <main class="py-4">
           @yield('content')
       </main>
-      
+
+      <div class="d-flex flex-column" id="content-wrapper">
+         <div id="content">
+         </div>
+         <footer class="bg-white sticky-footer">
+             <div class="container my-auto">
+                 <div class="text-center my-auto copyright"><span>Copyright © Proyecto-256 2021</span></div>
+             </div>
+         </footer>
+     </div>
+     <a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
     </div>
 
 
