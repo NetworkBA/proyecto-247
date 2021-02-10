ed80b83d3768584c9a7b30165d234b349dfe2667
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-09 19:31:40 -0600

    x

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,22 +3,7 @@
 @section('content')
 <body id="page-top">
 
-      <nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0">
-          <div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
-                  <div class="sidebar-brand-icon rotate-n-15"><i class="fas fa-laugh-wink"></i></div>
-                  <div class="sidebar-brand-text mx-3"><span>proyecto-256</span></div>
-              </a>
-              <hr class="sidebar-divider my-0">
-              <ul class="nav navbar-nav text-light" id="accordionSidebar">
-                  <li class="nav-item"><a class="nav-link active" href="index.html"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
-                  <li class="nav-item"><a class="nav-link" href="perfil"><i class="fas fa-user"></i><span>Profile</span></a></li>
-                  <li class="nav-item"><a class="nav-link" href="table.html"><i class="fas fa-table"></i><span>Table</span></a></li>
-                  <li class="nav-item"><a class="nav-link" href="login"><i class="far fa-user-circle"></i><span>Inico de Sesión</span></a></li>
-                  <li class="nav-item"><a class="nav-link" href="register.html"><i class="fas fa-user-circle"></i><span>Register</span></a></li>
-              </ul>
-              <div class="text-center d-none d-md-inline"><button class="btn rounded-circle border-0" id="sidebarToggle" type="button"></button></div>
 
-      </nav>
          <div class="d-flex flex-column" id="content-wrapper">
             <div id="content">
             </div>
