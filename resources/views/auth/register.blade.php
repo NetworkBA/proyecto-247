f774b6b94d8c98c1976d55a92eeb02a5930cc6d9
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-11 15:25:18 -0600

    x

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <body class="bg-gradient-primary">
-    <div class="container">
+    <div class="container" style="margin-top:10px" >
         <div class="card shadow-lg o-hidden border-0 my-5">
             <div class="card-body p-0">
                 <div class="row">
