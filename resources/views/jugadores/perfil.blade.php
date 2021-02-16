8000d37f527260903c3e08808b68a836868095b5
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-15 19:58:25 -0600

    x

--- resources/views/jugadores/perfil.blade.php
+++ resources/views/jugadores/perfil.blade.php
@@ -138,6 +138,18 @@
         <div class="card-body"></div>
     </div>
 
+    <!-- The Modal -->
+<div id="id01" class="w3-modal">
+  <div class="w3-modal-content">
+    <div class="w3-container">
+      <span onclick="document.getElementById('id01').style.display='none'"
+      class="w3-button w3-display-topright">&times;</span>
+      <p>Some text in the Modal..</p>
+      <p>Some text in the Modal..</p>
+    </div>
+  </div>
+</div>
+
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/chart.min.js"></script>
