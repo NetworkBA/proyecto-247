b7dc06bdd2c5b4370e6b6b21dc1e96726b6b4169
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-15 20:28:05 -0600

    x

--- resources/views/jugadores/perfil.blade.php
+++ resources/views/jugadores/perfil.blade.php
@@ -106,8 +106,30 @@
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                  <button class="btn btn-primary btn-sm" type="submit">Editar Datos Generales</button></div>
+                                  <button data-toggle="modal" data-target="#myModal" class="btn btn-primary btn-sm" type="submit">Editar Datos Generales</button></div>
+
                             </form>
+                            <!-- Modal -->
+                                    <div class="modal fade" id="myModal" role="dialog">
+                                      <div class="modal-dialog">
+
+                                        <!-- Modal content-->
+                                        <div class="modal-content">
+                                          <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                            <h4 class="modal-title">Modal Header</h4>
+                                          </div>
+                                          <div class="modal-body">
+                                            <p>Some text in the modal.</p>
+                                          </div>
+                                          <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                          </div>
+                                        </div>
+
+                                      </div>
+                                    </div>
+
                         </div>
                     </div>
                     <div class="card shadow">
