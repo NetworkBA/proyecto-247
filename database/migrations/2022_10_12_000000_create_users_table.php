5b035ced0e89f8a99df63c9f3f796423f48f11e9
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-15 22:59:33 -0600

    x

--- database/migrations/2022_10_12_000000_create_users_table.php
+++ database/migrations/2022_10_12_000000_create_users_table.php
@@ -19,17 +19,23 @@ class CreateUsersTable extends Migration
             $table->string('ApellidoP');
             $table->string('ApellidoM')->nullable;
             $table->date('FechaNacimiento');
-            $table->integer('id_TipoUsuario');
-            $table->integer('id_HistorialDeportivo');
-            $table->integer('id_HistorialMedico');
-            $table->integer('id_Equipo');
+            $table->integer('id_TipoUsuario')->unsigned();
+            $table->integer('id_HistorialDeportivo')->unsigned();
+            $table->integer('id_HistorialMedico')->unsigned();
+            $table->integer('id_Equipo')->unsigned()->nullable();;
             $table->string('imagedeusuario');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('id_TipoUsuario')->references('id')->on('TipodeUsuarios')->onDelete('cascade');
+            $table->foreign('id_HistorialDeportivo')->references('id')->on('HistorialDeportivo')->onDelete('cascade');
+            $table->foreign('id_HistorialMedico')->references('id')->on('HistorialMedico')->onDelete('cascade');
+            $table->foreign('id_Equipo')->references('id')->on('Equipos')->onDelete('set null');;
         });
+
     }
 
     /**
