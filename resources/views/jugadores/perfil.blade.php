a2a92658f8d2a7e587724def77bbe98eaaad1316
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-15 19:57:40 -0600

    x

--- resources/views/jugadores/perfil.blade.php
+++ resources/views/jugadores/perfil.blade.php
@@ -106,18 +106,7 @@
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                  <button onclick="document.getElementById('id01').style.display='block'"class="w3-button">Editar Datos Generales</button>
-                                </div>
-                                <!-- The Modal -->
-                                <div id="id01" class="w3-modal">
-                                  <div class="w3-modal-content">
-                                    <div class="w3-container">
-                                      <span onclick="document.getElementById('id01').style.display='none'"
-                                      class="w3-button w3-display-topright">&times;</span>
-                                      <p>Some text in the Modal..</p>
-                                      <p>Some text in the Modal..</p>
-                                    </div>
-                                  </div>
+                                  <button onclick="document.getElementById('id01').style.display='block'" class="btn btn-primary btn-sm" type="submit">Editar Datos Generales</button>
                                 </div>
                             </form>
                         </div>
