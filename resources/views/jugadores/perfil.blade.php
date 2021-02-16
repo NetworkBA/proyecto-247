19c8cc2c6718b45b996be5fd7727a016a8911ce6
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-15 20:25:46 -0600

    x

--- resources/views/jugadores/perfil.blade.php
+++ resources/views/jugadores/perfil.blade.php
@@ -106,10 +106,7 @@
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                  <button data-toggle="modal" data-target="#myModal" class="btn btn-primary btn-sm" type="submit">Editar Datos Generales</button>
-                                </div>
-
-
+                                  <button class="btn btn-primary btn-sm" type="submit">Editar Datos Generales</button></div>
                             </form>
                         </div>
                     </div>
@@ -139,28 +136,7 @@
         <div class="card-header py-3"></div>
         <div class="card-body"></div>
     </div>
-    <!-- Modal -->
-     <div class="modal fade" id="myModal" role="dialog">
-       <div class="modal-dialog">
 
-         <!-- Modal content-->
-         <div class="modal-content">
-           <div class="modal-header">
-             <button type="button" class="close" data-dismiss="modal">&times;</button>
-             <h4 class="modal-title">Modal Header</h4>
-           </div>
-           <div class="modal-body">
-             <p>Some text in the modal.</p>
-           </div>
-           <div class="modal-footer">
-             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-           </div>
-         </div>
-
-       </div>
-     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/chart.min.js"></script>
