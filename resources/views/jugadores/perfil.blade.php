a3535d907db738885b9c180e1a8e09e036554515
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-15 19:09:19 -0600

    x

--- resources/views/jugadores/perfil.blade.php
+++ resources/views/jugadores/perfil.blade.php
@@ -125,5 +125,12 @@
         <div class="card-header py-3"></div>
         <div class="card-body"></div>
     </div>
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/chart.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+    <script src="assets/js/theme.js"></script>
 </div>
 @endsection
