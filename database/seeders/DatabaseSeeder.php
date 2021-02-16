d4b6ceb87476391d41351d3e6c1b140d3d04dbbb
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-15 23:55:16 -0600

    x

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([TipodeUsuarios::class,]);
+        $this->call([TiposUsuarios::class,]);
     }
 }
