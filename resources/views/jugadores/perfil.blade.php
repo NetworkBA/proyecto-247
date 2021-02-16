364f855e9c55e49e2827db9d6d87b6043fa00ad6
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-15 20:49:04 -0600

    x

--- resources/views/jugadores/perfil.blade.php
+++ resources/views/jugadores/perfil.blade.php
@@ -117,7 +117,35 @@
 
                                   </div>
                                   <div class="modal-body">
-                                      ….
+                                        <div class="form-row">
+                                            <div class="col">
+                                                <div class="form-group">
+                                                  <label for="username"><strong>Nombre</strong></label>
+                                                  <input class="form-control" type="text" placeholder="{{Auth()->user()->name}}" name="username" disabled />
+                                                </div>
+                                            </div>
+                                            <div class="col">
+                                                <div class="form-group">
+                                                  <label for="email"><strong>Correo Electronico</strong>
+                                                </label><input class="form-control" type="email" placeholder="{{Auth()->user()->email}}" name="email" disabled/>
+                                              </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-row">
+                                            <div class="col">
+                                                <div class="form-group"><label for="first_name"><strong>Apellido Paterno</strong>
+                                                </label><input class="form-control" type="text" placeholder="{{Auth()->user()->ApellidoP}}" name="-ApellidoP" disabled/>
+                                              </div>
+                                            </div>
+                                            <div class="col">
+                                                <div class="form-group"><label for="last_name"><strong>Apellido Materno</strong>
+                                                </label><input class="form-control" type="text" placeholder="{{Auth()->user()->ApellidoM}}" name="ApellidoM" disabled/>
+                                              </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                          <button class="btn btn-primary btn-sm" type="submit" data-toggle="modal" data-target="#create" >Editar Datos Generales</button></div>
+                                </div>
                                   </div>
                                   <div class="modal-footer">
                                       <input type="submit" class="btn btn-primary" value="Guardar">
