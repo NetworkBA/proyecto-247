39cc182f14b63d3c0a7f118c686be1d2890bc2bc
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-18 13:57:07 -0600

    x

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,22 +20,22 @@
                                 <form method="POST" action="{{ route('login') }}">
                                   @csrf
                                     <div class="form-group">
-                                      <input class="form-control form-control-user" type="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." name="email">
+                                      <input class="form-control form-control-user" type="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Correo Electronico" name="email">
                                     </div>
                                     <div class="form-group">
-                                      <input class="form-control form-control-user" type="password" id="exampleInputPassword" placeholder="Password" name="password">
+                                      <input class="form-control form-control-user" type="password" id="exampleInputPassword" placeholder="Contraseña" name="password">
                                     </div>
                                     <div class="form-group">
                                         <div class="custom-control custom-checkbox small">
                                             <div class="form-check">
                                               <input class="form-check-input custom-control-input" type="checkbox" id="formCheck-1">
-                                              <label class="form-check-label custom-control-label" for="formCheck-1">Remember Me</label></div>
+                                              <label class="form-check-label custom-control-label" for="formCheck-1">Recordarme</label></div>
                                         </div>
-                                    </div><button class="btn btn-primary btn-block text-white btn-user" type="submit">Login</button>
+                                    </div><button class="btn btn-primary btn-block text-white btn-user" type="submit">Iniciar Sesión</button>
 
                                 </form>
-                                <div class="text-center"><a class="small" href="forgot-password.html">Forgot Password?</a></div>
-                                <div class="text-center"><a class="small" href="register.html">Create an Account!</a></div>
+                                <div class="text-center"><a class="small" href="">¿Olvidaste tu Contraseña?</a></div>
+                                <div class="text-center"><a class="small" href="register">Crear Cuenta</a></div>
                             </div>
                         </div>
                     </div>
