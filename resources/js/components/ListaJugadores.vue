e3876fdcbb190d6b33afea64d146a5c036522f4c
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-28 23:42:17 -0600

    x

--- resources/js/components/ListaJugadores.vue
+++ resources/js/components/ListaJugadores.vue
@@ -34,7 +34,7 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for ="Jugadores in jugador" :key="jugador.id">
+                            <tr v-for ="Jugadores in jugador" :key="jugador.id"> 
                                 <td>{{jugador.name + jugador.ApellidoP}}</td>
                                 <td></td>
                                 <td>Tokyo</td>
