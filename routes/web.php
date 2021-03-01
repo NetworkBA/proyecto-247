b702ac04ad9f37544543232f9c5e42cbc4e940a5
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-28 21:00:06 -0600

    x

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ Route::get('/perfil', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/Jugadores', function () {
+    return view('jugadores.perfil');
+});
