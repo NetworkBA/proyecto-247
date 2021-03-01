98552e013464103ab6afc04bbf7948090124403f
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-03-01 17:15:55 -0600

    x

--- resources/js/components/ListaJugadores.vue
+++ resources/js/components/ListaJugadores.vue
@@ -48,20 +48,9 @@
                                 <option>...</option>
                               </select>
                             </div>
-                            <div class="form-group col-md-2">
-                              <label for="inputZip">Zip</label>
-                              <input type="text" class="form-control" id="inputZip">
-                            </div>
                           </div>
-                          <div class="form-group">
-                            <div class="form-check">
-                              <input class="form-check-input" type="checkbox" id="gridCheck">
-                              <label class="form-check-label" for="gridCheck">
-                                Check me out
-                              </label>
-                            </div>
-                          </div>
-                          <button type="submit" class="btn btn-primary">Sign in</button>
+
+
                           </form>
                         </div>
                         <div class="modal-footer">
