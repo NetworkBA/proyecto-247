915d61230702682e65e78934225e9afaad2b2a91
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-03-01 17:11:56 -0600

    x

--- resources/js/components/ListaJugadores.vue
+++ resources/js/components/ListaJugadores.vue
@@ -11,8 +11,8 @@
                     <div class="modal-dialog">
                       <div class="modal-content">
                         <div class="modal-header">
-                          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <h5 class="modal-title" id="exampleModalLabel">Crear Jugador</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
                             <span aria-hidden="true">&times;</span>
                           </button>
                         </div>
@@ -20,13 +20,13 @@
                           ...
                         </div>
                         <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                          <button type="button" class="btn btn-primary">Save changes</button>
+                          <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                          <button type="button" class="btn btn-primary">Crear Jugador</button>
                         </div>
                       </div>
                     </div>
                   </div>
-
+                  <!-- Modal -->
             </div>
 
             <div class="card-body">
