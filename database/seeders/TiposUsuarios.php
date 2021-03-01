97ce75fb583c6c9f6a4788ee822388a4353b946f
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-03-01 01:29:47 -0600

    x

--- database/seeders/TiposUsuarios.php
+++ database/seeders/TiposUsuarios.php
@@ -30,6 +30,19 @@ class TiposUsuarios extends Seeder
     DB::table('TipodeUsuarios')->insert([
       'TipodeUsuarios' => 'Jugador',
     ]);
+    DB::table('TipodeUsuarios')->insert([
+      'TipodeUsuarios' => 'Jugador',
+    ]);
+    DB::table('HistorialDeportivo')->insert([
+      'id' => 0,
+    ]);
+    DB::table('HistorialMedico')->insert([
+      'id' => 0,
+    ]);
+    DB::table('users')->insert([
+     'name' => 'Ninguno', 'ApellidoP' => 'Ninguno','ApellidoM' => 'Ninguno','FechaNacimiento' => '01/01/1990','id_TipoUsuario' => 2,
+     'id_HistorialDeportivo' => 0,'id_HistorialMedico' => 0,'email' => 'ninguno@gmail.com','password' => 'patata12',
+   ]);
 
 
     }
