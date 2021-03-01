445bb63dba9c2771bd003691201e6483621e2169
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-03-01 17:32:45 -0600

    x

--- resources/js/components/ListaJugadores.vue
+++ resources/js/components/ListaJugadores.vue
@@ -50,23 +50,6 @@
                             <input class="form-control" type="date" value="2011-08-19" id="FechaNacimiento">
                           </div>
 
-
-
-                          <div class="form-row">
-                            <div class="form-group col-md-6">
-                              <label for="inputCity">City</label>
-                              <input type="text" class="form-control" id="inputCity">
-                            </div>
-                            <div class="form-group col-md-4">
-                              <label for="inputState">State</label>
-                              <select id="inputState" class="form-control">
-                                <option selected>Choose...</option>
-                                <option>...</option>
-                              </select>
-                            </div>
-                          </div>
-
-
                           </form>
                         </div>
                         <div class="modal-footer">
