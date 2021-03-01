b42c02cbf1eac6c110f31e7ae68e996ca7fc142a
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-03-01 01:46:37 -0600

    x

--- database/seeders/TiposUsuarios.php
+++ database/seeders/TiposUsuarios.php
@@ -42,14 +42,14 @@ class TiposUsuarios extends Seeder
     ]);
     DB::table('users')->insert([
      'name' => 'Ninguno', 'ApellidoP' => 'Ninguno','ApellidoM' => 'Ninguno','FechaNacimiento' => Carbon::parse('2000-01-01'),'id_TipoUsuario' => 2,
-     'id_HistorialDeportivo' => 1,'id_HistorialMedico' => 1,'email' => 'ninguno@gmail.com','password' => 'patata12','imagedeusuario' => 'ninguno',
+     'id_HistorialDeportivo' => 1,'id_HistorialMedico' => 1,'email' => 'ninguno@gmail.com','password' => 'patata12345678','imagedeusuario' => 'ninguno',
     ]);
     DB::table('Equipos')->insert([
       'Nombre' => 'ninguno','id_Entrenador' => 1,
     ]);
     DB::table('users')->insert([
      'name' => 'admi', 'ApellidoP' => 'Ninguno','ApellidoM' => 'Ninguno','FechaNacimiento' => Carbon::parse('2000-01-01'),'id_TipoUsuario' => 1,
-     'id_HistorialDeportivo' => 1,'id_HistorialMedico' => 1,'email' => 'admi@gmail.com','password' => 'patata12','imagedeusuario' => 'ninguno',
+     'id_HistorialDeportivo' => 1,'id_HistorialMedico' => 1,'email' => 'admi@gmail.com','password' => 'patata12345678','imagedeusuario' => 'ninguno',
     ]);
 
     }
