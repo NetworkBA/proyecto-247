d79ad7ed45144ee664732fb2537b65beb67020a0
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-03-01 00:40:37 -0600

    x

--- resources/js/components/ListaJugadores.vue
+++ resources/js/components/ListaJugadores.vue
@@ -10,12 +10,19 @@
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-6 text-nowrap">
-
+                        <div id="dataTable_length" class="dataTables_length" aria-controls="dataTable"><label>Show <select class="form-control form-control-sm custom-select custom-select-sm">
+                                    <option value="10" selected>10</option>
+                                    <option value="25">25</option>
+                                    <option value="50">50</option>
+                                    <option value="100">100</option>
+                                </select></label></div>
                     </div>
-
+                    <div class="col-md-6">
+                        <div class="text-md-right dataTables_filter" id="dataTable_filter"><label><input type="search" class="form-control form-control-sm" aria-controls="dataTable" placeholder="Search" /></label></div>
+                    </div>
                 </div>
                 <div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
-                    <table class="table my-0" id="myTable">
+                    <table class="table my-0" id="dataTable">
                         <thead>
                             <tr>
                                 <th>Nombre</th>
@@ -27,31 +34,41 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for ="jugador in Jugadores" :key="jugador.id" >
-                                <td>{{jugador.name + ' ' + jugador.ApellidoP + ' '+ jugador.ApellidoM +  ''}}</td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
+                              <tr v-for ="jugador in Jugadores" :key="jugador.id" >
+                                  <td>{{jugador.name + ' ' + jugador.ApellidoP + ' '+ jugador.ApellidoM +  ''}}</td>
+                                  <td></td>
+                                  <td></td>
+                                  <td></td>
+                                  <td></td>
+                                  <td></td>
+                              </tr>
                         </tbody>
                         <tfoot>
                             <tr>
                                 <td><strong>Nombre</strong></td>
-                                <td><strong>Equipo</strong></td>
-                                <td><strong>Posición</strong></td>
-                                <td><strong>Edad</strong></td>
-                                <td><strong>Fecha de Inicio date</strong></td>
-                                <td><strong>Calificación</strong></td>
+                                <td><strong>Position</strong></td>
+                                <td><strong>Office</strong></td>
+                                <td><strong>Age</strong></td>
+                                <td><strong>Start date</strong></td>
+                                <td><strong>Salary</strong></td>
                             </tr>
                         </tfoot>
                     </table>
                 </div>
                 <div class="row">
-
+                    <div class="col-md-6 align-self-center">
+                        <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite">Showing 1 to 10 of 27</p>
+                    </div>
                     <div class="col-md-6">
-
+                        <nav class="d-lg-flex justify-content-lg-end dataTables_paginate paging_simple_numbers">
+                            <ul class="pagination">
+                                <li class="page-item disabled"><a class="page-link" href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+                                <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+                            </ul>
+                        </nav>
                     </div>
                 </div>
             </div>
@@ -61,7 +78,6 @@
 </template>
 
 <script>
-  import datables from 'datatables'
     export default {
         mounted() {
             console.log('Component Lista Jugadores Montada.')
@@ -88,18 +104,13 @@
           }
         },
           methods:{
-
-                  mytable(){
-                    $(document).ready( function () {
-                        $('#myTable').DataTable();
-                    } );
-                  },
-
-                  getListaJugadores(){
-                      this.mytable()
-                      var urlEstud = "ListaJugadores";
-                      axios.get(urlEstud).then(response =>  { this.Jugadores = response.data});
-                  }
+          getListaJugadores(){
+        var urlEstud = "ListaJugadores";
+        axios.get(urlEstud).then(response =>
+        {
+          this.Jugadores = response.data
+        });
           }
+    }
     }
 </script>
