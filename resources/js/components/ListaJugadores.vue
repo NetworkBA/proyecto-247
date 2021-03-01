00cc37af3bd4e482402749550378b9c4910e26fa
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-03-01 01:00:43 -0600

    x

--- resources/js/components/ListaJugadores.vue
+++ resources/js/components/ListaJugadores.vue
@@ -8,9 +8,21 @@
             </div>
 
             <div class="card-body">
-
+                <div class="row">
+                    <div class="col-md-6 text-nowrap">
+                        <div id="dataTable_length" class="dataTables_length" aria-controls="dataTable"><label>Show <select class="form-control form-control-sm custom-select custom-select-sm">
+                                    <option value="10" selected>10</option>
+                                    <option value="25">25</option>
+                                    <option value="50">50</option>
+                                    <option value="100">100</option>
+                                </select></label></div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="text-md-right dataTables_filter" id="dataTable_filter"><label><input type="search" class="form-control form-control-sm" aria-controls="dataTable" placeholder="Search" /></label></div>
+                    </div>
+                </div>
                 <div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
-                    <table   id="myTable">
+                    <table class="table my-0" id="dataTable">
                         <thead>
                             <tr>
                                 <th>Nombre</th>
@@ -43,7 +55,22 @@
                         </tfoot>
                     </table>
                 </div>
-
+                <div class="row">
+                    <div class="col-md-6 align-self-center">
+                        <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite">Showing 1 to 10 of 27</p>
+                    </div>
+                    <div class="col-md-6">
+                        <nav class="d-lg-flex justify-content-lg-end dataTables_paginate paging_simple_numbers">
+                            <ul class="pagination">
+                                <li class="page-item disabled"><a class="page-link" href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+                                <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -51,7 +78,6 @@
 </template>
 
 <script>
-    import datable from "datatables"
     export default {
         mounted() {
             console.log('Component Lista Jugadores Montada.')
@@ -78,21 +104,12 @@
           }
         },
           methods:{
-
-
-          gettables()
-          {
-              $(document).ready( function () {
-              $('#myTable').DataTable();
-              } );
-          },
           getListaJugadores(){
         var urlEstud = "ListaJugadores";
         axios.get(urlEstud).then(response =>
         {
           this.Jugadores = response.data
         });
-        this.gettables()
           }
     }
     }
