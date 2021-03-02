0077a0bc96e328e49581ac2e31704c07197688dc
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-03-02 15:20:38 -0600

    x

--- database/seeders/TiposUsuarios.php
+++ database/seeders/TiposUsuarios.php
@@ -41,12 +41,22 @@ class TiposUsuarios extends Seeder
     DB::table('HistorialMedico')->insert([
       'id' => 0,
     ]);
+    DB::table('HistorialDeportivo')->insert([
+      'id' => 1,
+    ]);
+    DB::table('HistorialMedico')->insert([
+      'id' => 1,
+    ]);
     DB::table('Liga')->insert([
       'Nombre' => 'a',
     ]);
     DB::table('users')->insert([
      'name' => 'Ninguno', 'ApellidoP' => 'Ninguno','ApellidoM' => 'Ninguno','FechaNacimiento' => Carbon::parse('2000-01-01'),'id_TipoUsuario' => 2,
      'id_HistorialDeportivo' => 1,'id_HistorialMedico' => 1,'email' => 'ninguno@gmail.com','password' => Hash::make('patata12345678'),'imagedeusuario' => 'ninguno',
+    ]);
+    DB::table('users')->insert([
+     'name' => 'Entrenador', 'ApellidoP' => 'Ninguno','ApellidoM' => 'Ninguno','FechaNacimiento' => Carbon::parse('2000-01-01'),'id_TipoUsuario' => 2,
+     'id_HistorialDeportivo' => 2,'id_HistorialMedico' => 2,'email' => 'entrenador@gmail.com','password' => Hash::make('patata12345678'),'imagedeusuario' => 'ninguno',
     ]);
     DB::table('Equipos')->insert([
       'Nombre' => 'Ninguno','id_Entrenador' => 1,'id_Liga' => 1,
