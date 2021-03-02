3b664b83f1cabce74d9e63ee3694c4760195fb9c
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-03-02 16:28:41 -0600

    x

--- app/Http/Controllers/equipos.php
+++ app/Http/Controllers/equipos.php
@@ -29,26 +29,17 @@ class equipos extends Controller
 
     public function Crearequipo(Request $request)
     {
-        
-            $data = request()->validate([
-                'name' => ['required', 'string', 'max:255', 'unique:equipo'],
-                'id_Liga' => ['required', 'numeric'],
-                'id_Entrenador' => ['required', 'numeric' ],
-            ]);
+      $data = request()->validate([
+          'name' => ['required', 'string', 'max:255'],
+          'id_Liga' => ['required', 'numeric'],
+          'id_Entrenador' => ['required', 'numeric' ],
+      ]);
 
-            $equipo = new equipo();
-            $equipo->Nombre = $data['name'];
-            $equipo->id_Entrenador = $data['id_Entrenador'];
-            $equipo->id_Liga = $data['id_Liga'];
-            $equipo->save();
-
-            $entrendador = User::find($data['id_Entrenador']);
-            $entrendador->id_Equipo = $equipo->id;
-            $entrendador->save();
-
-            return true;
-
-
+      return equipo::create([
+        'Nombre' => $data['name'],
+        'id_Entrenador' => $data['id_Entrenador'],
+        'id_Liga' => $data['id_Liga'],
+      ]);
 
 
     }
