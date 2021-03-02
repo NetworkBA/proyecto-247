2e89c3381a5b87b9d77649b9ca5ed43cd3df2511
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-03-01 18:18:01 -0600

    x

--- app/Http/Controllers/jugador.php
+++ app/Http/Controllers/jugador.php
@@ -17,29 +17,28 @@ class jugador extends Controller
         return $Jugadores;
     }
 
-    public function Crearjugador(Request $request)
-    {
-      $data = request()->validate([
-          'name' => ['required', 'string', 'max:255'],
-          'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-      ]);
-      $historialdeportivo = new historial_deportivo;
-      $historialdeportivo->save();
-      $historialmedico = new historial_medico;
-      $historialmedico->save();
+          public function Crearjugador(Request $request)
+          {
+            $data = request()->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            ]);
+            $historialdeportivo = new historial_deportivo;
+            $historialdeportivo->save();
+            $historialmedico = new historial_medico;
+            $historialmedico->save();
 
-      return User::create([
-          'name' => $data['name'],
-          'ApellidoP' => $data['ApellidoP'],
-          'ApellidoM' => $data['ApellidoM'],
-          'FechaNacimiento' =>$data['FechaNacimiento'],
-          'email' => $data['email'],
-          'password' => Hash::make('patata12345678'),
-          'id_TipoUsuario' => 5,
-          'id_HistorialDeportivo' => $historialdeportivo['id'],
-          'id_HistorialMedico' => $historialmedico['id'],
-          'imagedeusuario' => 'assets/img/avatars/avatar5.jpeg',
-      ]);
-  }
+            return User::create([
+                'name' => $data['name'],
+                'ApellidoP' => $data['ApellidoP'],
+                'ApellidoM' => $data['ApellidoM'],
+                'FechaNacimiento' =>$data['FechaNacimiento'],
+                'email' => $data['email'],
+                'password' => Hash::make('patata12345678'),
+                'id_TipoUsuario' => 5,
+                'id_HistorialDeportivo' => $historialdeportivo['id'],
+                'id_HistorialMedico' => $historialmedico['id'],
+                'imagedeusuario' => 'assets/img/avatars/avatar5.jpeg',
+            ]);
+        }
     }
-}
