15d2daaebdaed0d9a5f87ecce6c1a7beb1f6a608
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-03-02 15:17:18 -0600

    x

--- app/Http/Controllers/entrenadores_controller.php
+++ app/Http/Controllers/entrenadores_controller.php
@@ -10,7 +10,7 @@ class entrenadores_controller extends Controller
 {
     public function ListaEntrenadores()
     {
-      return User::select('users.id','users.name','users.ApellidoP','users.ApellidoM')->rightJoin('Equipos','users.id','=','Equipos.id_Entrenador')->where('users.id_TipoUsuario','=','2',')->get();
+      return User::select('users.id','users.name','users.ApellidoP','users.ApellidoM')->rightJoin('Equipos','users.id','=','Equipos.id_Entrenador')->where('users.id_TipoUsuario','=','2')->get();
 
     }
 }
