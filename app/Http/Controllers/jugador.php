8b1f7f71c60d6752f4b73c255d65bdc3109db5c1
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-03-15 20:53:47 -0600

    x

--- app/Http/Controllers/jugador.php
+++ app/Http/Controllers/jugador.php
@@ -16,7 +16,7 @@ class jugador extends Controller
     public  function ListaJugadores()
     {
         $Jugadores = User::select('users.id' ,'users.id_Equipo','users.name','users.ApellidoP','users.ApellidoM')
-        ->join('users','Equipos.id','=','users.id_Equipo')->join('Liga','Equipos.id_Liga','=','Liga.id')->where('users.id_TipoUsuario','5')->get();
+        ->where('users.id_TipoUsuario','5')->get();
         return $Jugadores;
     }
 
